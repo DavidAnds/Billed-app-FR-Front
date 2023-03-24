92d681467614705e78c0b611b92231a1232f32d2
David ANDRIASON <davidandriason7@gmail.com>
2023-03-24 11:07:25 +0100

    src/__tests__/Bills.js Get integration test added

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,14 +5,18 @@
 import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import NewBillUI from "../views/NewBillUI.js";
-import { bills } from "../fixtures/bills.js";
+
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import Bills from "../containers/Bills.js";
 import userEvent from '@testing-library/user-event'
+import mockStore from "../__mocks__/store";
+import { bills } from "../fixtures/bills.js";
 
 import router from "../app/Router.js";
 import DashboardFormUI from "../views/DashboardFormUI.js";
+
+jest.mock("../app/store", () => (mockStore));
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -21,10 +25,10 @@ describe("Given I am connected as an employee", () => {
         value: localStorageMock,
       });
       window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
       );
       const root = document.createElement("div");
       root.setAttribute("id", "root");
@@ -40,10 +44,10 @@ describe("Given I am connected as an employee", () => {
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen
-        .getAllByText(
-          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
-        )
-        .map((a) => a.innerHTML);
+          .getAllByText(
+              /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+          )
+          .map((a) => a.innerHTML);
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
@@ -57,7 +61,7 @@ describe("Given I am connected as an employee", () => {
         }))
         document.body.innerHTML = BillsUI({ data: bills });
         const onNavigate = (pathname) => {
-            document.body.innerHTML = ROUTES({ pathname });
+          document.body.innerHTML = ROUTES({ pathname });
         };
         const store = null;
         const billsCurrent = new Bills({ document, onNavigate, store, localStorage: window.localStorage });
@@ -69,7 +73,7 @@ describe("Given I am connected as an employee", () => {
 
         const newBill = screen.getByTestId("form-new-bill");
         expect(newBill).toBeTruthy();
-      } );
+      });
     });
 
     describe("When I click on the eye icon", () => {
@@ -94,8 +98,66 @@ describe("Given I am connected as an employee", () => {
         expect(modal).toBeTruthy();
       })
     })
+  })
+})
+
+// test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to Bills Page", () => {
+        test("fetches bills from mock API GET", async () => {
+            localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+            const root = document.createElement("div");
+            root.setAttribute("id", "root");
+            document.body.append(root);
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+            await waitFor(() => screen.getByText("Mes notes de frais"))
+            const table = await screen.getByTestId("tbody");
+            expect(table).toBeTruthy();
+            const eye = await screen.getAllByTestId("icon-eye");
+            expect(eye).toBeTruthy();
+        });
+    })
+
+    describe("When an error occur on API", () => {
+
+        beforeEach(() => {
+            jest.spyOn(mockStore, "bills")
+            Object.defineProperty(window, "localStorage", { value: localStorageMock })
+            window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.appendChild(root)
+            router()
+        })
 
 
+        test("fetches bills from an API and fails with 404 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                    list: () => {
+                       return  Promise.reject(new Error("Erreur 404"))
+                    }
+                }
+            })
+            window.onNavigate(ROUTES_PATH.Bills)
+            await new Promise(process.nextTick)
+            const message = await screen.getByText(/Erreur 404/)
+            expect(message).toBeTruthy()
+        })
 
-  });
-});
+        test("fetches messages from an API and fails with 500 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+                return {
+                    list: () => {
+                       return  Promise.reject(new Error("Erreur 500"))
+                    }
+                }
+            })
+            window.onNavigate(ROUTES_PATH.Bills)
+                await new Promise(process.nextTick)
+                const message = await screen.getByText(/Erreur 500/)
+                expect(message).toBeTruthy()
+        })
+    });
+})
