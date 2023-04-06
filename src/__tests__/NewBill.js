a25001bbf736c6fd118aa53429736bdb06096741
David ANDRIASON <davidandriason7@gmail.com>
2023-04-06 12:07:32 +0200

    src/__tests__/NewBill.js test integration bug corrected

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -11,16 +11,18 @@ import userEvent from '@testing-library/user-event'
 import mockStore from "../__mocks__/store.js";
 import router from "../app/Router.js";
 
+
 // Créer une fonction mock pour remplacer window.alert
 const mockAlert = jest.fn();
 window.alert = mockAlert;
 
+jest.mock("../app/store.js", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    describe('When I change file input', () => {
+    describe('When I change file input with a good format', () => {
       jest.spyOn(window, "alert")
-      test("Then it should upload a new file", () => {
+      test("Then it should upload a new file and display the file name", () => {
         const html = NewBillUI()
         document.body.innerHTML = html
 
@@ -42,13 +44,33 @@ describe("Given I am connected as an employee", () => {
         const NewCurrentBill = new NewBill({ document, onNavigate, localStorage: window.localStorage, store })
 
         const handleChangeFile = jest.fn(NewCurrentBill.handleChangeFile)
-        const file = screen.getByTestId("file")
-        file.addEventListener("change", handleChangeFile)
-        userEvent.upload(file, new File(["hello"], "hello.png", { type: "image/png" }))
+        const fileInput = screen.getByTestId("file")
+        const file = new File(["hello"], "hello.png", { type: "image/png" })
+        fileInput.addEventListener("change", handleChangeFile)
+        userEvent.upload(fileInput, file)
         expect(handleChangeFile).toHaveBeenCalled()
+        expect(fileInput.files[0]).toStrictEqual(file)
+      })
+      test('then a file should be created', () => {
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+            "user",
+            JSON.stringify({
+              type: "Employee",
+            })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+        const fileInput = screen.getByTestId("file")
+        const file = new File(["hello"], "hello.png", { type: "image/png" })
+        userEvent.upload(fileInput, file)
+        expect(fileInput.files[0]).toStrictEqual(file)
       })
     })
-
     describe('When I submit form', () => {
       test("Then it should create a new bill", async () => {
         const html = NewBillUI()
@@ -82,7 +104,35 @@ describe("Given I am connected as an employee", () => {
         await userEvent.upload(file, new File(["hello"], "hello.png", { type: "image/png" }))
         await userEvent.click(screen.getByTestId("btn-send-bill"))
         expect(handleSubmit).toHaveBeenCalled()
-        expect(screen.getByText("Mes notes de frais")).toBeTruthy()
+      })
+      test("Then I should Navigate to Bills page", async () => {
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+            "user",
+            JSON.stringify({
+              type: "Employee",
+            })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+        window.onNavigate(ROUTES_PATH.NewBill);
+        await userEvent.type(screen.getByTestId("expense-type"), "Restaurant")
+        await userEvent.type(screen.getByTestId("expense-name"), "Restau U")
+        await userEvent.type(screen.getByTestId("amount"), "200")
+        await userEvent.type(screen.getByTestId("datepicker"), "2023-07-22")
+        await userEvent.type(screen.getByTestId("vat"), "20")
+        await userEvent.type(screen.getByTestId("pct"), "20")
+        await userEvent.type(screen.getByTestId("commentary"), "un commentaire")
+        const file = screen.getByTestId("file")
+        await userEvent.upload(file, new File(["hello"], "hello.png", { type: "image/png" }))
+        await userEvent.click(screen.getByTestId("btn-send-bill"))
+        await waitFor(() => {
+          expect(screen.getByText("Mes notes de frais")).toBeTruthy()
+        })
       })
     })
   })
@@ -103,8 +153,10 @@ describe("Given I am a user connected as Employee", () => {
       })
         test("Then it fails with 404 message error", async () => {
           console.error = jest.fn()
-          mockStore.bills.mockImplementationOnce(() => {
+          const allMethods= mockStore.bills()
+          mockStore.bills.mockImplementation(() => {
             return {
+                ...allMethods,
                 update: () => {
                   return Promise.reject(new Error("Erreur 404"))
                 },
@@ -121,9 +173,33 @@ describe("Given I am a user connected as Employee", () => {
             const file = screen.getByTestId("file")
             await userEvent.upload(file, new File(["hello"], "hello.png", { type: "image/png" }))
             await userEvent.click(screen.getByTestId("btn-send-bill"))
-            await waitFor(() => {
-              expect(console.error).toHaveBeenCalledWith(new Error("Error 404"))
-            })
+            await new Promise(process.nextTick)
+            expect(console.error).toHaveBeenCalledWith(new Error('Erreur 404'))
+        })
+        test("Then it fails with 500 message error", async () => {
+          console.error = jest.fn()
+          const allMethods= mockStore.bills()
+          mockStore.bills.mockImplementation(() => {
+            return {
+                ...allMethods,
+                update: () => {
+                  return Promise.reject(new Error("Erreur 500"))
+                },
+            }
+          })
+          window.onNavigate(ROUTES_PATH.NewBill)
+            await userEvent.type(screen.getByTestId("expense-type"), "Restaurant")
+            await userEvent.type(screen.getByTestId("expense-name"), "Restau U")
+            await userEvent.type(screen.getByTestId("amount"), "200")
+            await userEvent.type(screen.getByTestId("datepicker"), "2023-07-22")
+            await userEvent.type(screen.getByTestId("vat"), "20")
+            await userEvent.type(screen.getByTestId("pct"), "20")
+            await userEvent.type(screen.getByTestId("commentary"), "un commentaire")
+            const file = screen.getByTestId("file")
+            await userEvent.upload(file, new File(["hello"], "hello.png", { type: "image/png" }))
+            await userEvent.click(screen.getByTestId("btn-send-bill"))
+            await new Promise(process.nextTick)
+            expect(console.error).toHaveBeenCalledWith(new Error('Erreur 500'))
         })
     })
   })
